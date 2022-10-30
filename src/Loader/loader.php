748247bf6d2566212d67934251d2491f3b223268
Denis Solodukhin <den-sidnay@yandex.ru>
2022-10-30 20:49:23 +0300

    add all

--- src/Loader/loader.php
+++ src/Loader/loader.php
@@ -4,10 +4,11 @@ namespace Loader\Loader;
 
 use function Loader\Formatter\formatUrlToFileName;
 
-const ROOT_PATH = __DIR__ . "/../..";
+const ROOT_PATH = __DIR__ . "/../../result/";
 
 function loadPage($url, $path, $client)
 {
+    createResultDir();
     $content =  $client->get($url, ['http_errors' => false]);
     if ($content->getStatusCode() != '200') {
         return "Link no valid!";
@@ -17,5 +18,12 @@ function loadPage($url, $path, $client)
         mkdir(ROOT_PATH . $path);
     }
     file_put_contents(ROOT_PATH . "{$path}/{$fileName}", $content->getBody()->getContents());
-    return "Saved in {$path}/{$fileName}" . PHP_EOL;
+    return "Saved in /result/{$path}/{$fileName}" . PHP_EOL;
+}
+
+function createResultDir()
+{
+    if (!is_dir(__DIR__ . "/../../result/")) {
+        mkdir(__DIR__ . "/../../result/");
+    }
 }
--- tests/LoaderTest.php
+++ tests/LoaderTest.php
@@ -1,1 +1,35 @@
 <?php
+
+namespace Loader\Tests;
+
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Client;
+
+use function Loader\Loader\loadPage;
+
+class LoaderTest extends TestCase
+{
+    private Client $client;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->client = new Client();
+    }
+
+    function testCorrectLoader() : void
+    {
+        $stub = $this->createMock(Client::class);
+        $this->assertEquals("Link no valid!", loadPage("https://fakeLink", "/test", $stub));
+    }
+
+    function testInCorrectLoader() : void
+    {
+        $url = "https://ru.hexlet.io/courses";
+        $path = "/test";
+
+        $this->assertEquals(
+            "Saved in {$path}/ru-hexlet-io-courses.html\n",
+            loadPage($url, $path, $this->client));
+    }
+}
